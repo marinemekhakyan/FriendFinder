51e09edff14898b0ef8acf32f5195ddd23f52453
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-08 11:54:28 -0700

    server file added

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("../routing/apiRoutes")(app);
+require("../routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(PORT);
+})
