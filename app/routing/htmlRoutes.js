117089762cbefc44a0546f9e87f1afb6b98f2800
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-08 11:56:43 -0700

    updating html routes path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,10 +4,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     app.get("/home", function (req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
 }
