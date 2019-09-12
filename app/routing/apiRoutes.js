8cd8ff4f30fc4a3f216bcf6f36608fe5b4183873
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-11 21:33:53 -0700

    getting json ron back from submitting the form

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,30 @@
 var friends = require ("../data/friends")
 
 module.exports = function(app){
-app.get("/api/friends", function(req, res) {
-    res.json(friends)
-});
+    app.get("/api/friends", function(req, res) {
+        res.json(friends)
+    });
 
-app.post("/api/friends", function(req, res){
+    app.post("/api/friends", function(req, res){
 
-    var surveyResult = [];
+        var surveyResult = req.body.scores;
+        console.log(req.body);
 
-    var bestMatch = [];
-    var minDif = 0;
+        var bestMatch = [];
+        var minDif = 0;
 
-    for (var i = 0; i < friends.length; i ++) {
-        var dif = 0;
-        for (var j = 0; j < surveyResult.length; j ++){
+        for (var i = 0; i < friends.length; i ++) {
+            var dif = 0;
+            for (var j = 0; j < surveyResult.length; j ++){
 
-            dif +=  Math.abs(friends[i].scores[j] - surveyResult[j]);
-        }
-        if ( i === 0) {
-            bestMatch = [friends[i].name, friends[i].photo];
-            minDif = dif;
-        }
-        else {
-            if (dif < minDif) {
+                dif +=  Math.abs(friends[i].scores[j] - surveyResult[j]);
+            }
+            if ( i === 0 || dif < minDif) {
                 bestMatch = [friends[i].name, friends[i].photo];
                 minDif = dif;
             }
-
+        
         }
-    
-    }
         res.json(bestMatch);
-})
+    })
 }
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("../routing/apiRoutes")(app);
-require("../routing/htmlRoutes")(app);
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log(PORT);
+    console.log("App listening on PORT: " + PORT);
 })
