45b723c37221085dffd0642972badb1c2739f579
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-14 14:59:09 -0700

    got modal working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,6 +13,11 @@ module.exports = function(app){
         var bestMatch = [];
         var minDif = 0;
 
+        var responseData = {
+            newUser: req.body,
+            bestMatch: null,
+        }
+
         for (var i = 0; i < friends.length; i ++) {
             var dif = 0;
             for (var j = 0; j < surveyResult.length; j ++){
@@ -20,11 +25,11 @@ module.exports = function(app){
                 dif +=  Math.abs(friends[i].scores[j] - surveyResult[j]);
             }
             if ( i === 0 || dif < minDif) {
-                bestMatch = [friends[i].name, friends[i].photo];
-                minDif = dif;
+                responseData.bestMatch = friends[i]; // [friends[i].name, friends[i].photo];
+                minDif = dif; 
             }
         
          }
-        res.json(bestMatch);
+        res.json(responseData);
     });
 }
